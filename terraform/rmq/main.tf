e2235b5a9575e211122de0418b76761a5ed1bbc2
EC2 Default User <ec2-user@ip-172-31-81-134.ec2.internal>
2021-03-12 18:33:51 +0000

    terraform files and directories

--- terraform/rmq/main.tf
+++ terraform/rmq/main.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "rmq" {
     ami = "ami-07ebfd5b3428b6f4d"
     instance_type = "t2.micro"
     key_name = "rabbitmq"
-    vpc_security_group_ids = ["sg-064ed5b607e81598b"]
+    vpc_security_group_ids = ["sg-05cb1aa9531a53201"]
 
     tags = {
         Name = var.name
